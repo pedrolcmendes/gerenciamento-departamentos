1c62ea15d6f40676fea6c0485701b92446ae2993
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-24 17:06:15 -0300

    add entity Departamento JPA

---
+++ departamentos/src/main/java/com/exercicio/departamentos/entity/Departamento.java
@@ -0,0 +1,29 @@
+package com.exercicio.departamentos.entity;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Departamento")
+@Table(name = "tb_departamento")
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_departamento")
+    private String nome;
+
+    @Column(name = "localizacao_departamento")
+    private String localizacao;
+
+    public Departamento() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getLocalizacao() { return localizacao; }
+    public void setLocalizacao(String localizacao) { this.localizacao = localizacao; }
+}
