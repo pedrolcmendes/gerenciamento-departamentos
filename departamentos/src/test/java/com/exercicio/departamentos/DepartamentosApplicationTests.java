c852e1c53979d5706f4b316f1a9d688c54633376
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-24 17:04:25 -0300

    primeiro commit

---
+++ departamentos/src/test/java/com/exercicio/departamentos/DepartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.departamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
